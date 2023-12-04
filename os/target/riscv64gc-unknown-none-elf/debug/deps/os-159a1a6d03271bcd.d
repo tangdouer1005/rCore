ce5a7eb52cd1e06e1bf281bab1202fd8a10b0a22
td <884746853@qq.com>
2023-12-04 06:33:50 +0000

    remove target and Cargo.lock

--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-159a1a6d03271bcd.d
+++
@@ -1,7 +0,0 @@
-/home/td/Desktop/rcore/os/target/riscv64gc-unknown-none-elf/debug/deps/libos-159a1a6d03271bcd.rmeta: src/main.rs src/lang_items.rs src/entry.asm
-
-/home/td/Desktop/rcore/os/target/riscv64gc-unknown-none-elf/debug/deps/os-159a1a6d03271bcd.d: src/main.rs src/lang_items.rs src/entry.asm
-
-src/main.rs:
-src/lang_items.rs:
-src/entry.asm:
