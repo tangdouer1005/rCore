e0b21ba3e771b305e8678bc5b5dc7330a53921fc
td <884746853@qq.com>
2023-12-05 09:06:59 +0000

    create a fn stack, call rust_main

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,5 +2,21 @@
 #![no_std]
 mod lang_items;
 
-use core::arch::global_asm;
+use core::{arch::global_asm, ptr::write_volatile};
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() {
+    bss_clear();
+    loop{}
+}
+
+fn bss_clear() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize .. ebss as usize).for_each(|addr|{
+        unsafe {(addr as *mut u8).write_volatile(0);}
+    })
+}
