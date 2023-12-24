b7b7a5ff14d021b3380e30b3219c47d87ed5d38b
td <884746853@qq.com>
2023-12-24 09:01:16 +0000

    add frame_allocator mod to manage free page

--- os/src/config.rs
+++ os/src/config.rs
@@ -11,3 +11,5 @@ pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
+
+pub const MEMORY_END: usize = 0x80800000;
--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,8 +1,13 @@
 mod heap_allocator;
 mod address;
+mod frame_allocator;
+
+pub use address::{PhysAddr, PhysPageNum, VirtAddr, VirtPageNum};
 
 
 pub fn init(){
     heap_allocator::init_heap();
     heap_allocator::heap_test();
+    frame_allocator::init_frame_allocator();
+    frame_allocator::frame_allocator_test();
 }
